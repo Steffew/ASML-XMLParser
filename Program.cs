1ba45fe38c27dc9b05800d90de6481b9d74b7ea4
Gijs <g.verdonk@icloud.com>
2023-09-27 11:29:31 +0200

    SQL connectie toegevoegd.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Data.SqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,6 +13,29 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+//SQL connection
+using (SqlConnection connection = new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")))
+{
+    try
+    {
+        connection.Open();
+    }
+    catch 
+    {
+        throw new Exception("Database connection error. (Don't forget the VPN)");
+    }
+
+
+    string sql = "SELECT @@VERSION";
+    using (SqlCommand command = new SqlCommand(sql, connection))
+    {
+        string version = command.ExecuteScalar().ToString();
+        Console.WriteLine($"SQL version is: {version}");
+    }
+
+    connection.Close();
 }
 
 app.UseHttpsRedirection();
