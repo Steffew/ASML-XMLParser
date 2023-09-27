eeb225a23eafb5c408e41a6cbe2e68876fc78b40
Steffew <xgenyofficial@gmail.com>
2023-09-27 11:24:41 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASMLXMLParser.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
