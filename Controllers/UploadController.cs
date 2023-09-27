799abf1a0f27530388f223e7877c5004ebecc340
Gijs <g.verdonk@icloud.com>
2023-09-27 14:50:09 +0200

    Added dashboard and upload page.

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,28 @@
+﻿using ASMLXMLParser.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASMLXMLParser.Controllers
+{
+    public class UploadController : Controller
+    {
+        private readonly ILogger<DashboardController> _logger;
+
+        public UploadController(ILogger<DashboardController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
