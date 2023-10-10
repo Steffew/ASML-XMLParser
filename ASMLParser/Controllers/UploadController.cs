61dbd28339dbaabbbbe8aa8d1143cda85a204098
Gijs <g.verdonk@icloud.com>
2023-10-10 10:23:35 +0200

    Added Upload/Create controller (POST).

--- ASMLParser/Controllers/UploadController.cs
+++ ASMLParser/Controllers/UploadController.cs
@@ -18,6 +18,23 @@ namespace ASMLXMLParser.Controllers
             return View();
         }
         
+        // POST: Upload/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UploadFile(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View("Index");
+            }
+        }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
