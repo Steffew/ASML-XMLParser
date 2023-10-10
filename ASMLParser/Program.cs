3e8ff4ce946e1d521b39d6385a6c577c44d75665
Gijs Verdonk <g.verdonk@icloud.com>
2023-10-10 15:02:15 +0200

    Update Program.cs
    Removed SQL connection in Program.cs

--- ASMLParser/Program.cs
+++ ASMLParser/Program.cs
@@ -14,27 +14,6 @@ if (!app.Environment.IsDevelopment())
 }
 
 //SQL connection
-//using (SqlConnection connection = new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")))
-//{
-//    try
-//    {
-//        connection.Open();
-//    }
-//    catch 
-//    {
-//        throw new Exception("Database connection error. (Don't forget the VPN)");
-//    }
-
-
-//    string sql = "SELECT @@VERSION";
-//    using (SqlCommand command = new SqlCommand(sql, connection))
-//    {
-//        string version = command.ExecuteScalar().ToString();
-//        Console.WriteLine($"SQL version is: {version}");
-//    }
-
-//    connection.Close();
-//}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
