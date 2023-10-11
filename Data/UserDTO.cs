04807b041282c47d63b23450f00dbaf638032032
Marleen <49445100+aMiyukii@users.noreply.github.com>
2023-10-11 09:39:16 +0200

    DTO class added
    remove incorrect class
    
    User and usercreation DTO's
    
    remove usercreation

---
+++ Data/UserDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    internal class UserDTO
+    {
+        //name
+        //email
+        //password
+    }
+}
