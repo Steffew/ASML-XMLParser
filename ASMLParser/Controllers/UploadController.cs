504bfaa4fdd5c6577d0d73eee93bc40dcad8b44a
FI502527 <502527@student.fontys.nl>
2023-10-11 14:58:38 +0200

    Upload pagina basis + drag en drop

--- ASMLParser/Controllers/UploadController.cs
+++ ASMLParser/Controllers/UploadController.cs
@@ -7,10 +7,11 @@ namespace ASMLXMLParser.Controllers
     public class UploadController : Controller
     {
         private readonly ILogger<DashboardController> _logger;
-
-        public UploadController(ILogger<DashboardController> logger)
+        private IWebHostEnvironment hostEnv;
+        public UploadController(ILogger<DashboardController> logger, IWebHostEnvironment env)
         {
             _logger = logger;
+            hostEnv = env;
         }
 
         public IActionResult Index()
@@ -22,6 +23,22 @@ namespace ASMLXMLParser.Controllers
         {
             return View();
         }
+        //public async Task<IActionResult> Upload(IFormFile file)
+        //{
+        //    var fileDic = "Files";
+        //    string filePath = Path.Combine(hostEnv.WebRootPath, fileDic);
+        //    if(!Directory.Exists(filePath))
+        //    {
+        //        Directory.CreateDirectory(filePath);
+        //    }
+        //    var fileName = file.FileName;
+        //    filePath = Path.Combine(filePath, fileName);
+        //    using (FileStream fs = System.IO.File.Create(filePath))
+        //    {
+        //        file.CopyTo(fs); 
+        //    }
+        //    return RedirectToAction("Index");
+        //}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
