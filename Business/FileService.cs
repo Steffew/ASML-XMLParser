3b00b6435093a4f7b8b8101df2c92e2d94d3b945
Steffew <xgenyofficial@gmail.com>
2023-10-11 13:42:15 +0200

    List every event and parameter

--- Business/FileService.cs
+++ Business/FileService.cs
@@ -8,16 +8,22 @@ namespace Business
         {
             XmlDocument document = new XmlDocument();
             document.Load(stream);
-            
+
             XmlNamespaceManager nsManager = new XmlNamespaceManager(document.NameTable);
-            
-            nsManager.AddNamespace("ns","Cimetrix.EDAConnect.E134-0707");
-            
-            XmlNodeList? eventNodes = document.SelectNodes("/ns:DCP/ns:Event", nsManager);
-            
+            nsManager.AddNamespace("ns", "Cimetrix.EDAConnect.E134-0707");
+
+            XmlNodeList eventNodes = document.SelectNodes("/ns:DCP/ns:Event", nsManager);
+
             foreach (XmlNode eventNode in eventNodes)
             {
                 Console.WriteLine($"Event: {eventNode.Attributes["Id"].Value}");
+                
+                foreach (XmlNode parameterNode in eventNode.SelectNodes("ns:Parameters/ns:Parameter", nsManager))
+                {
+                    Console.WriteLine($"Parameter: {parameterNode.Attributes["Name"].Value} / {parameterNode.Attributes["SourceId"].Value}");
+                }
+                
+                Console.WriteLine();
             }
         }
 
