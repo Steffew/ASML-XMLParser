116cc5af0afc8180b470eba3a27448923513c7e7
Steffew <xgenyofficial@gmail.com>
2023-10-11 10:14:58 +0200

    Update FileService.cs

--- Business/FileService.cs
+++ Business/FileService.cs
@@ -6,17 +6,34 @@ namespace Business
     {
         public void ReadFileData(Stream stream) //TODO: return het model bijv.
         {
-            XmlDocument doc = new XmlDocument();
-            doc.Load(stream);
+            // XmlDocument doc = new XmlDocument();
+            // doc.Load(stream);
+            //
+            // XmlNodeList xmlNodeList = doc.SelectNodes("/people/person");
+            //
+            // foreach (XmlNode xmlNode in xmlNodeList)
+            // {
+            //     string personName = xmlNode["name"].InnerText;
+            //     string personAge = xmlNode["age"].InnerText;
+            //     Console.WriteLine($"{personName} + {personAge}");
+            // }
+            
+            XmlDocument document = new XmlDocument();
+            document.Load(stream);
 
-            XmlNodeList xmlNodeList = doc.SelectNodes("/people/person");
-
-            foreach (XmlNode xmlNode in xmlNodeList)
+            XmlNodeList? eventNodes = document.SelectNodes("/DCP");
+            if (eventNodes != null)
             {
-                string personName = xmlNode["name"].InnerText;
-                string personAge = xmlNode["age"].InnerText;
-                Console.WriteLine($"{personName} + {personAge}");
+                foreach (XmlNode eventNode in eventNodes)
+                {
+                    Console.WriteLine($"{eventNode.Attributes["Id"].Value}");
+                }
             }
+            else
+            {
+                Console.WriteLine("No event nodes!");
+            }
+
         }
 
         public void SendFileData() //TODO: invoer is het model bijv.
