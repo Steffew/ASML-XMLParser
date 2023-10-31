9f23144fa17393a5338088cc5e7afaeea40855d6
FI502527 <502527@student.fontys.nl>
2023-10-31 12:21:40 +0100

    Front-end combability back-end

--- ASMLParser/Controllers/UploadController.cs
+++ ASMLParser/Controllers/UploadController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Business;
+using System.IO;
 
 namespace ASMLXMLParser.Controllers
 {
@@ -7,10 +8,11 @@ namespace ASMLXMLParser.Controllers
     {
         private readonly ILogger<DashboardController> _logger;
         private readonly FileService FileService = new FileService();
-
-        public UploadController(ILogger<DashboardController> logger)
+        private IWebHostEnvironment hostEnv;
+        public UploadController(ILogger<DashboardController> logger, IWebHostEnvironment env)
         {
             _logger = logger;
+            hostEnv = env;
         }
 
         public IActionResult Index()
@@ -57,7 +59,67 @@ namespace ASMLXMLParser.Controllers
         
         public IActionResult Upload()
         {
+            ViewBag.Message = TempData["Result"];
             return View();
+        }
+        public IActionResult SaveFiles(IFormFile file)
+        {
+            if (file != null && file.Length != 0)
+            {
+                var fileDic = "Files";
+                string FilePath = Path.Combine(hostEnv.WebRootPath, fileDic);
+                if (!Directory.Exists(FilePath))
+                {
+                    Directory.CreateDirectory(FilePath);
+                }
+                var fileName = file.FileName;
+                var filePath = Path.Combine(FilePath, fileName);
+                using (FileStream fs = System.IO.File.Create(filePath))
+                {
+                    file.CopyTo(fs);
+                }
+            }
+            return NoContent();
+        }
+        public IActionResult ProcessFiles()
+        {
+            var fileDic = "Files";
+            string filePath = Path.Combine(hostEnv.WebRootPath, fileDic);
+            DirectoryInfo directory = new DirectoryInfo(filePath);
+            FileInfo[] files = directory.GetFiles();
+            bool onlyXml = true;
+            foreach (FileInfo file in files)
+            {
+                if (file.Extension != ".xml")
+                {
+                    onlyXml = false;
+                }
+            }
+            if (onlyXml)
+            {
+                foreach (FileInfo file in files)
+                {
+                    try
+                    {
+
+                        FileStream stream = file.OpenRead();
+                        FileService.RetrieveFileData(stream);
+                        stream.Close();
+                        file.Delete();
+                        TempData["Result"] = "succes";
+                    }
+                    catch
+                    {
+                        TempData["Result"] = "An error occurred while processing the file.";
+                    }
+                }
+                return RedirectToAction("Upload");
+            }
+            else
+            {
+                TempData["Result"] = "noxml";
+                return RedirectToAction("Upload");
+            }
         }
     }
 }
