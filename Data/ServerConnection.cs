0557568797dfa4adf1c1372492475611ee42e47a
Mauricio <samsonmauricio@gmail.com>
2023-11-01 10:32:15 +0100

    Upload begonnen

--- Data/ServerConnection.cs
+++ Data/ServerConnection.cs
@@ -7,14 +7,17 @@ namespace Data
         private static string ConnectionString = "Data Source=mssqlstud.fhict.local;Initial Catalog=dbi458166_asmleda;Persist Security Info=True;User ID=dbi458166_asmleda;Password=Mr36733duBG2";
         private SqlConnection sqlConnection = new(ConnectionString);
 
-        public void UploadData()
+        public void UploadData(SqlCommand uploadCommand)
         {
-
+            sqlConnection.Open();
+            uploadCommand.ExecuteNonQuery();
+            sqlConnection.Close();
         }
 
         public void LoadData()
         {
-
+            sqlConnection.Open();
+            sqlConnection.Close();
         }
 
     }
