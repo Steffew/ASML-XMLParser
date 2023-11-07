1ccc4716a5bcbd944b1565abfdae07dbbb3e31d5
G.Bronts <gabibronts@gmail.com>
2023-11-07 11:33:48 +0100

    Test query

--- Data/Upload.cs
+++ Data/Upload.cs
@@ -2,6 +2,8 @@
 using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +12,15 @@ namespace Data
 {
     public class Upload
     {
-        public void UploadMachine(List<MachineDTO> machines)
-        {
 
+        
+
+        public void UploadMachine(MachineDTO machine)
+        {
+            MachineDTO invoer = new MachineDTO(1, "test");
+            string query = $"INSERT INTO Machine (MachineName) VALUES ({invoer.MachineName});";
+            SqlCommand command = new SqlCommand(query);
+            
         }
 
 
