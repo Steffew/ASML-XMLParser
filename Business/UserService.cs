9d0660d6207e62942370a52501eb7889e65db11e
Gijs Verdonk <g.verdonk@icloud.com>
2023-11-14 11:28:09 +0100

    Add GetById (User)

--- Business/UserService.cs
+++ Business/UserService.cs
@@ -19,13 +19,28 @@ namespace Business
 			UserDTO userDto = new UserDTO();
 			//userDto = repository.GetUser();
 			
-			if (userDto.Role == "Admin")
+			if (userDto.Role.Name == "Admin")
 			{
 				return true;
 			}else
 			{
 				return false;
 			}
+		}
+
+		public User GetById(int id)
+		{
+			UserDTO userDto = new UserDTO();
+			//userDto = UserRepository.GetById(int id); // TODO: repository methode toevoegen.
+
+			// RoleDTO roleDto = new RoleDTO();
+			// user.Role = new Role(roleDto.Id, roleDto.Name); 
+			
+			User user = new User(userDto.Id, userDto.Name);
+
+			user.Role = new Role(userDto.Role.Id, userDto.Role.Name); 
+			
+			return user;
 		}
 	}
 }
