3ea9e69222f7a319aa9e11293e7f05d616a16c3c
FI502527 <502527@student.fontys.nl>
2023-11-15 13:30:30 +0100

    Admin panel on profile page

---
+++ ASMLParser/Models/RoleViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ASMLXMLParser.Models
+{
+    public class RoleViewModel
+    {
+        public int Id;
+        public string Name;
+
+        public RoleViewModel(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
