840c7baadd5951288be27791ff1ab373d423a771
FI502527 <502527@student.fontys.nl>
2023-11-22 12:30:15 +0100

    Profile view model toevoeging

---
+++ ASMLParser/Models/ProfileViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ASMLXMLParser.Models
+{
+    public class ProfileViewModel
+    {
+        public UserViewModel CurrentUser { get; set; }
+        public List<UserViewModel> Users { get; set; }
+        public ProfileViewModel(UserViewModel currentUser, List<UserViewModel> users)
+        {
+            CurrentUser = currentUser;
+            Users = users;
+        }
+    }
+}
