267a3ecb9eab5b5f5145e081ca99b3933fbe0b46
Mauricio <samsonmauricio@gmail.com>
2023-11-29 12:50:52 +0100

    TestIfUploadedFilesGetSaved works and passes the test

--- ASML Unit Tests/UploadTests.cs
+++ ASML Unit Tests/UploadTests.cs
@@ -5,7 +5,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.Extensions.Logging;
+using Microsoft.JSInterop.Implementation;
 using Moq;
+using System.Text;
+using System.Xml;
 
 namespace ASML_Unit_Tests
 {
@@ -15,13 +18,32 @@ namespace ASML_Unit_Tests
         private readonly FileService FileService = new FileService();
         private Mock<IWebHostEnvironment> hostEnv;
         private UploadController uploadcontroller;
+        private Mock<IFormFile> mockfile;
 
         [SetUp]
         public void Setup()
         {
             _logger = new();
             hostEnv = new();
+            mockfile = new();
+            MockFileSetup();
             uploadcontroller = new(_logger.Object, hostEnv.Object);
+            
+        }
+
+        public void MockFileSetup()
+        {
+            var filecontent = "Unit Test";
+            var filename = "UnitTest.XML";
+            var ms = new MemoryStream();
+            var writer = new StreamWriter(ms);
+            writer.Write(filecontent);
+            writer.Flush();
+            ms.Position = 0;
+
+            mockfile.Setup(_ => _.OpenReadStream()).Returns(ms);
+            mockfile.Setup(_ => _.FileName).Returns(filename);
+            mockfile.Setup(_ => _.Length).Returns(ms.Length);
         }
 
         [Test]
@@ -37,18 +59,38 @@ namespace ASML_Unit_Tests
             var result = uploadcontroller.Index() as ViewResult;
 
             // Assert
-            Assert.IsNull(result.ViewName);
+            Assert.IsInstanceOf<IActionResult>(result);
         }
 
         [Test]
         public void TestIfUploadedFilesGetSaved()
         {
             // Arrange
+            var file = mockfile.Object;
+            var filecontent = "Unit Test";
+            var contentBytes = Encoding.UTF8.GetBytes(filecontent);
+            var stream = new MemoryStream(contentBytes);
+            mockfile.Setup(f => f.OpenReadStream()).Returns(stream);
+            hostEnv.Setup(_ => _.WebRootPath).Returns("~\\ASMLParser\\wwwroot");
+            string filepath = Path.Combine("~\\ASMLParser\\wwwroot" + "Files");
 
-            // Act
+            // Act 
+            var result = uploadcontroller.SaveFiles(file) as ViewResult;
 
             // Assert
-            Assert.
+            Assert.IsNull(result);
+
+            using (var reader = new StreamReader(stream))
+            {
+                var actualContent = reader.ReadToEnd();
+                Assert.AreEqual(filecontent, actualContent);
+            }
+        }
+
+        [Test]
+        public void test1() 
+        {
+
         }
     }
 }
