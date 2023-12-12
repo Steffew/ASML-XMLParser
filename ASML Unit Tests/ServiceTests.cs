e5197c27e44b54a4ecd39b5b42d57e9e7bf44d88
Mauricio <samsonmauricio@gmail.com>
2023-12-12 15:00:15 +0100

    Added tests, fixed db connection issues

--- ASML Unit Tests/ServiceTests.cs
+++ ASML Unit Tests/ServiceTests.cs
@@ -12,18 +12,11 @@ namespace ASML_Unit_Tests
         [SetUp]
         public void Setup()
         {
-            testFileService = new();
-            SetupStream();
-            
+            testFileService = new();           
         }
 
-        public void SetupStream()
+        public void SetupStream(string FileContent)
         {
-            string FileContent = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\r\n<DCP Id=\"779075fc-38b0-4d01-ba1d-6cd340da9bd9\" Name=\"UnitTest\" " +
-                "Persistent=\"True\" Interval=\"0\" Defined=\"True\" DefinedBy=\"Me\" TimeDefined=\"2012-05-21T13:56:07.028+02:00\"\r\n   " +
-                "xmlns=\"Cimetrix.EDAConnect.E134-0707\">\r\n   <Description />\r\n   <Event Id=\"UnitTest\" SourceId=\"UnitTestEvent\">\r\n      " +
-                "<Parameters>\r\n         <Parameter Name=\"UnitTestParameter\" SourceId=\"UnitTestParameter\" />\r\n         " +
-                "<Parameter Name=\"UnitTestParameter2\" SourceId=\"UnitTestParameter2\" />\r\n      </Parameters>\r\n   </Event>\r\n</DCP>\r\n\r\n";
             byte[] contentBytes = Encoding.UTF8.GetBytes(FileContent);
             memoryStream = new(contentBytes);
         }
@@ -55,6 +48,14 @@ namespace ASML_Unit_Tests
         [Test]
         public void FileServiceRetrievesFileData()
         {
+            // Arrange
+            string FileContent = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\r\n<DCP Id=\"779075fc-38b0-4d01-ba1d-6cd340da9bd9\" Name=\"UnitTest\" " +
+            "Persistent=\"True\" Interval=\"0\" Defined=\"True\" DefinedBy=\"Me\" TimeDefined=\"2012-05-21T13:56:07.028+02:00\"\r\n   " +
+            "xmlns=\"Cimetrix.EDAConnect.E134-0707\">\r\n   <Description />\r\n   <Event Id=\"UnitTest\" SourceId=\"UnitTestEvent\">\r\n      " +
+            "<Parameters>\r\n         <Parameter Name=\"UnitTestParameter\" SourceId=\"UnitTestParameter\" />\r\n         " +
+            "<Parameter Name=\"UnitTestParameter2\" SourceId=\"UnitTestParameter2\" />\r\n      </Parameters>\r\n   </Event>\r\n</DCP>\r\n\r\n";
+            SetupStream(FileContent);
+
             // Act
             testFileService.RetrieveFileData(memoryStream);
 
@@ -62,6 +63,34 @@ namespace ASML_Unit_Tests
             Assert.IsTrue(AssertDatabase());
         }
 
-        
+        [Test]
+        public void FileServiceWithoutNamespace()
+        {
+            // Arrange
+            string FileContent = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\r\n<DCP Id=\"779075fc-38b0-4d01-ba1d-6cd340da9bd9\" Name=\"UnitTest\" " +
+                "Persistent=\"True\" Interval=\"0\" Defined=\"True\" DefinedBy=\"Me\" TimeDefined=\"2012-05-21T13:56:07.028+02:00\">\r\n   " +
+                "<Description />\r\n   <Event Id=\"UnitTest\" SourceId=\"UnitTestEvent\">\r\n      <Parameters>\r\n         " +
+                "<Parameter Name=\"UnitTestParameter\" SourceId=\"UnitTestParameter\" />\r\n         " +
+                "<Parameter Name=\"UnitTestParameter2\" SourceId=\"UnitTestParameter2\" />\r\n      </Parameters>\r\n   </Event>\r\n</DCP>\r\n\r\n";
+            SetupStream(FileContent);
+
+            // Assert
+            Assert.Throws<System.NullReferenceException>(() => testFileService.RetrieveFileData(memoryStream));
+        }
+        [Test]
+        public void FileServiceWithoutEventsOrParameters()
+        {
+            // Arrange
+            string FileContent = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\r\n<DCP Id=\"779075fc-38b0-4d01-ba1d-6cd340da9bd9\" Name=\"UnitTest\" " +
+                "Persistent=\"True\" Interval=\"0\" Defined=\"True\" DefinedBy=\"Me\" TimeDefined=\"2012-05-21T13:56:07.028+02:00\"\r\n   " +
+                "xmlns=\"Cimetrix.EDAConnect.E134-0707\">\r\n   <Description />\r\n</DCP>";
+            SetupStream(FileContent);
+
+            // Act
+            testFileService.RetrieveFileData(memoryStream);
+
+            // Assert
+            Assert.IsTrue(AssertDatabase());
+        }
     }
 }
